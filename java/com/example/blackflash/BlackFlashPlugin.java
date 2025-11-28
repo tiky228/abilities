3decebd2631e71710677cd03cd9edab3ea71619c
tiky! <ytegorezem@gmail.com>
2025-11-28 18:52:29 +1000

    Add Reverse Cursed Technique ability

--- java/com/example/blackflash/BlackFlashPlugin.java
+++ java/com/example/blackflash/BlackFlashPlugin.java
@@ -2,25 +2,34 @@ package com.example.blackflash;
 
 import org.bukkit.NamespacedKey;
 import org.bukkit.command.PluginCommand;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BlackFlashPlugin extends JavaPlugin {
 
     private BlackFlashAbility blackFlashAbility;
+    private ReverseCursedTechniqueAbility reverseCursedTechniqueAbility;
     private NamespacedKey blackFlashAxeKey;
 
     @Override
     public void onEnable() {
         this.blackFlashAxeKey = new NamespacedKey(this, "blackflash_axe");
         this.blackFlashAbility = new BlackFlashAbility(this, blackFlashAxeKey);
+        this.reverseCursedTechniqueAbility = new ReverseCursedTechniqueAbility(this);
         getServer().getPluginManager().registerEvents(new BlackFlashListener(blackFlashAbility), this);
+        getServer().getPluginManager().registerEvents(new ReverseCursedTechniqueListener(reverseCursedTechniqueAbility), this);
         registerCommands();
-        getLogger().info("Black Flash ability loaded.");
+        getLogger().info("Black Flash and Reverse Cursed Technique abilities loaded.");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("Black Flash ability disabled.");
+        if (reverseCursedTechniqueAbility != null) {
+            for (Player player : getServer().getOnlinePlayers()) {
+                reverseCursedTechniqueAbility.clearState(player);
+            }
+        }
+        getLogger().info("Black Flash abilities disabled.");
     }
 
     private void registerCommands() {
